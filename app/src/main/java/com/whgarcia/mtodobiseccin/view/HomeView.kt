9a9ce1751d1552c51eb59c8d1f76515eb9984680
WiliamGarcia814 <whgarcia.dev@gmail.com>
2024-09-18 15:02:11 -0600

    refactor: bottomSheetDialog
    Changes in implementation

--- app/src/main/java/com/whgarcia/mtodobiseccin/view/HomeView.kt
+++ app/src/main/java/com/whgarcia/mtodobiseccin/view/HomeView.kt
@@ -13,10 +13,6 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Slider
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
